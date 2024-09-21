008252c0cbb927ef6e270d80e118e51a96f84cde
madotsuki <castillo.matiasg@gmail.com>
2024-09-21 11:09:47 -0300

    Funcion qr implementada se procede a testeos y union a tablas

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Usuario } from '../models/usuario.model';
+import jsQR from 'jsqr';
 
 @Component({
   selector: 'app-inicio',
@@ -8,7 +9,11 @@ import { Usuario } from '../models/usuario.model';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage {
+  @ViewChild('video') private video!: ElementRef;
+  @ViewChild('canvas') private canvas!: ElementRef;
+
   usuario: Usuario;
+  escaneando = false;
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
@@ -16,7 +21,6 @@ export class InicioPage {
 
     if (!this.usuario) {
       console.error('No se recibieron datos del usuario');
-      // Maneja el caso donde no hay usuario
     }
   }
 
@@ -24,7 +28,50 @@ export class InicioPage {
     console.log('Navegando a Mis Datos con usuario:', this.usuario);
     this.router.navigate(['/mis-datos'], {
       state: { usuario: this.usuario }
-      
+    });
+  }
+
+  comenzarEscaneo() {
+    this.escaneando = true;
+    this.iniciarCamara();
+  }
+
+  async iniciarCamara() {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
+    this.video.nativeElement.srcObject = mediaStream;
+    this.video.nativeElement.setAttribute('playsinline', 'true');
+    this.video.nativeElement.play();
+    requestAnimationFrame(this.verificarVideo.bind(this));
+  }
+
+  async verificarVideo() {
+    if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
+      const w = this.video.nativeElement.videoWidth;
+      const h = this.video.nativeElement.videoHeight;
+      this.canvas.nativeElement.width = w;
+      this.canvas.nativeElement.height = h;
+      const context = this.canvas.nativeElement.getContext('2d');
+      context.drawImage(this.video.nativeElement, 0, 0, w, h);
+      const imgData = context.getImageData(0, 0, w, h);
+      const qrCode = jsQR(imgData.data, w, h);
+
+      if (qrCode) {
+        this.escaneando = false;
+        this.mostrarDatosQROrdenados(qrCode.data);
+      } else {
+        requestAnimationFrame(this.verificarVideo.bind(this));
+      }
+    } else {
+      requestAnimationFrame(this.verificarVideo.bind(this));
+    }
+  }
+
+  mostrarDatosQROrdenados(datosQR: string) {
+    const asistencia = JSON.parse(datosQR);
+    console.log('Datos de asistencia:', asistencia);
+    // Aquí podrías navegar a la página de mi-clase, pasando los datos de asistencia.
+    this.router.navigate(['/mi-clase'], {
+      state: { asistencia }
     });
   }
 }
